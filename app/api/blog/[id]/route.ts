dfc42a0c3247dc6cdbdbf376b2c751b7c40132fc
akashelhance <akashelhance@gmail.com>
2023-10-21 06:25:38 +0530

    dynamic route

---
+++ app/api/blog/[id]/route.ts
@@ -0,0 +1,9 @@
+export const GET = async(req: Request,res: Response) =>{
+    return new Response("Trying out the next api creating dynamic id")
+};
+
+
+
+export const DELETE = async(req: Request,res: Response) =>{
+    return new Response("Trying out the next api creating dynamic id and trying out the delete verb")
+};
---
+++ app/api/blog/route.ts
@@ -0,0 +1,8 @@
+export const GET = async(req: Request,res: Response) =>{
+    return new Response("Trying out the next api creating")
+};
+
+
+export const POST = async(req: Request,res: Response) =>{
+    return new Response("Trying out the next api creating POST route")
+};
